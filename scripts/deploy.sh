89c24a73d5604606cb4192900265acb3369c7460
jhs851 <jhs851@naver.com>
2019-08-19 15:10:46 +0900

    Open the server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/app/codeship/hyungseok
-aws s3 cp s3://hyungseok-deploy/.env.prod ./.env && docker-compose up -d --no-deps
+cd /home/ubuntu/app/codeship/hyungseok && aws s3 cp s3://hyungseok-deploy/.env.prod ./.env && docker-compose up -d --no-deps --build
