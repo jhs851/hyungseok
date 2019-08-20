98272d5d545a508904d0a28735e17a3c8c2c4ebe
jhs851 <jhs851@naver.com>
2019-08-20 17:31:03 +0900

    Implement social login registration and authentication: Github 2

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,7 +16,7 @@ class EventServiceProvider extends ServiceProvider
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
             \App\SocialiteProviders\Naver\NaverExtendSocialite::class,
             \App\SocialiteProviders\Kakao\KakaoExtendSocialite::class,
-            \App\SocialiteProviders\GitHub\GitHubExtendSocialite::class,
+            \App\SocialiteProviders\GitHub\GithubExtendSocialite::class,
             \SocialiteProviders\Google\GoogleExtendSocialite::class,
             \SocialiteProviders\Facebook\FacebookExtendSocialite::class,
         ],
