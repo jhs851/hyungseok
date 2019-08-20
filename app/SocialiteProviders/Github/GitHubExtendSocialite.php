eaa04e74f6b6b0b23ebfbf63858e8ceac2fabb12
jhs851 <jhs851@naver.com>
2019-08-20 17:37:12 +0900

    Implement social login registration and authentication: Github 2

--- app/SocialiteProviders/Github/GitHubExtendSocialite.php
+++ app/SocialiteProviders/Github/GitHubExtendSocialite.php
@@ -4,7 +4,7 @@ namespace App\SocialiteProviders\Github;
 
 use SocialiteProviders\Manager\SocialiteWasCalled;
 
-class GitHubExtendSocialite
+class GithubExtendSocialite
 {
     /**
      * Execute the provider.
