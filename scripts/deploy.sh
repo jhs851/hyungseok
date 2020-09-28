96c5f294fd939b975ef24ebeee712aa119034002
jhs851 <jhs851@naver.com>
2020-09-28 15:37:25 +0900

    S3 경로 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/app/codeship/hyungseok && aws s3 cp s3://hyungseok-webservices-deploy/.env.prod ./.env && docker-compose up -d --force-recreate
+cd /home/ubuntu/app/codeship/hyungseok && aws s3 cp s3://hyungseok-deploy/.env.prod ./.env && docker-compose up -d --force-recreate
